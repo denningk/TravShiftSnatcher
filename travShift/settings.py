677ee452d33355450af1d05affa9fe3043c3856b
Keith Denning <denningk18@mail.wlu.edu>
2018-02-21 00:34:14 -0500

    collected static files

--- travShift/settings.py
+++ travShift/settings.py
@@ -118,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main/templates/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), '/static/', ]
